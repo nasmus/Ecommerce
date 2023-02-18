b9bdfe278000aa851e9dd17d3fcbe0c4b4d70723
nasmus <nasmus.shahadat@gmail.com>
2023-02-19 04:26:49 +0600

    complex state management

--- clint/src/screen/HomeScreen.js
+++ clint/src/screen/HomeScreen.js
@@ -1,12 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger'
+
+const reducer = (state,action) => {
+  switch(action.type){
+    case 'FATCH_REQUEST':
+      return{...state, loading:true};
+    case 'FATCH_SUCCESS':
+      return{...state, products: action.payload, loading:false };
+    case 'FATCH_FAILLED':
+      return{...state, loading:false, error:action.payload};
+    default:
+      return state;
+  }
+}
+
 function HomeScreen() {
-  const [products,setProducts] = useState([]);
+  const [{loading, error, products}, dispatch] = useReducer( logger(reducer),{
+    products:[],
+    loading:true,
+    error:''
+  })
+  //const [products,setProducts] = useState([]); // if we use useState then this is use it
   useEffect(()=>{
     const fatchData = async() => {
-      const result = await axios.get(`/api/products`);
-      setProducts(result.data);
+      dispatch({ type:'FATCH_REQUEST' })
+      try {
+        const result = await axios.get(`/api/products`);
+        dispatch({type:'FATCH_SUCCESS', payload:result.data})
+      } catch (err) {
+        dispatch({ type: 'FATCH_FAILLED', payload:err.message })
+      }
+      
+      //setProducts(result.data); // if we use useState then we use it
     };
     fatchData();
   },[])
@@ -15,6 +42,11 @@ function HomeScreen() {
         <h1>Featured Product</h1>
         <div className="products">
           {
+            loading? (
+              <div>Lodding ...</div>
+            ) : error ? (
+              <div>{error}</div>
+            ): (
             
               products.map(product => (
               <div className="product" key={product.id} >
@@ -33,7 +65,7 @@ function HomeScreen() {
                   <button>Add to cart</button>
                 </div>
               </div>
-            ))
+            )))
           }
         </div>
     </div>
