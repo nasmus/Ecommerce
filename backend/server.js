ab75b12b73e6c40dea951f95d355e4694e60513a
nasmus <nasmus.shahadat@gmail.com>
2023-02-24 04:26:00 +0600

    mongodb connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = express();
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL ).then(()=> {
+    console.log('connect to db')
+}).catch((err)=> {
+    console.log(err.message);
+})
 
 app.get('/api/products', (req,res) => {
     res.send(data.products);
