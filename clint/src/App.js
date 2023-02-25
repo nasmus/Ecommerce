323a6802f9a23cb3209764b36cd9ada6ac9c636d
nasmus <nasmus.shahadat@gmail.com>
2023-02-26 00:06:11 +0600

    signin and error message complete

--- clint/src/App.js
+++ clint/src/App.js
@@ -1,23 +1,33 @@
 import { useContext } from 'react';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import HomeScreen from './screen/HomeScreen';
 import ProductScreen from './screen/ProductScreen';
 import "./App.css";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Badge from 'react-bootstrap/esm/Badge';
 import { Store } from './Store';
 import CartScreen from './screen/CartScreen';
 import SignInScreen from './screen/SignInScreen';
-//import LinkContainer from 'react-router-bootstrap/LinkContainer';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
 
 function App() {
-  const {state} = useContext(Store);
-  const {cart} = state;
+  const {state, dispatch:ctxDispatch} = useContext(Store);
+  const {cart,userInfo} = state;
+
+  const signOutHandler = () =>{
+    ctxDispatch({ type:'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo ')
+  }
+
   return (
     <BrowserRouter>
     <div className="d-flex flex-column">
+      <ToastContainer position='bottom-center' limit={1} />
       <header>
         <Navbar bg="dark" variant="dark">
         <Container>
@@ -37,6 +47,23 @@ function App() {
                 )
               }
             </Link>
+            { userInfo ? (
+              <NavDropdown title={userInfo.name} id="basic-nav-dropdown" >
+                <LinkContainer to="/profile" >
+                  <NavDropdown.Item>User Profoile</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/profile" >
+                  <NavDropdown.Item>Order Histori</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Divider />
+                <Link className='dropdown-item' to='#signOut' onClick={signOutHandler} >
+                  Sign Out
+                </Link>
+                
+              </NavDropdown>
+            ) : (
+              <Link className="nav__link" to="/signin" >Sing In</Link>
+            ) }
           </Nav>
         </Container>
       </Navbar>
--- clint/src/screen/SignInScreen.js
+++ clint/src/screen/SignInScreen.js
@@ -1,26 +1,61 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Button from 'react-bootstrap/esm/Button'
 import Container from 'react-bootstrap/esm/Container'
 import Form from 'react-bootstrap/Form'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import  Axios  from 'axios'
+import { Store } from '../Store';
+import { toast } from 'react-toastify';
+import {getError} from '../utils';
 
 function SignInScreen() {
+    const navigate = useNavigate();
     const { search } = useLocation();
     const redirectUrl = new URLSearchParams(search).get('redirect');
     const redirect = redirectUrl ? redirectUrl : '/';
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const { state, dispatch:ctxDispatch } = useContext(Store)
+    const {userInfo} = state;
+
+    const submitHendler = async(e) => {
+        e.preventDefault();
+        try{
+            const { data } = await Axios.post('/api/users/signin', {
+                email,
+                password
+            });
+            console.log(data);
+            ctxDispatch({type:'USER_SIGNIN', payload: data});
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            navigate(redirect || '/');
+
+        } catch (err){
+            toast.error(getError(err));
+            
+        }
+    }
+
+    useEffect(() => {
+        if(userInfo){
+            navigate(redirect)
+        }
+
+    },[navigate,redirect,userInfo]);
+
   return (
     <div>
         <Container className='small__container' >
             <h1>Sign In</h1>
-            <Form>
+            <Form onSubmit={submitHendler} >
                 <Form.Group className="md-3" controlId="email"  >
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" required />
+                    <Form.Control type="email" onChange={(e) => setEmail(e.target.value)} required />
                 </Form.Group>
                 <Form.Group className="md-3" controlId="password"  >
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" required />
+                    <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} required />
                 </Form.Group>
                 <div md={3}>
                     <Button type='submit'>
