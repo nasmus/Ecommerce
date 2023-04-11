8882e6f63cad6ca0c5029d17f62f7f73ae744f1a
nasmus <nasmus.shahadat@gmail.com>
2023-04-11 12:44:51 +0600

    order address api

--- backend/routes/sellerRouter/OrderRouter/sellerOrderRouter.js
+++ backend/routes/sellerRouter/OrderRouter/sellerOrderRouter.js
@@ -15,5 +15,19 @@ sellerOrderRouter.get(
         res.send(order);
     })
 )
+sellerOrderRouter.get(
+    '/orderaddress',
+    isAuth,
+    isSeller,
+    expressAsyncHandler( async(req,res) => {
+        const userId = req.user._id;
+        const orderAddress = await Order.distinct("shippingAddress").where({"orderItems.seller": userId})
+        if(orderAddress){
+            res.status(200).send(orderAddress)
+        } else {
+            res.status(404).send({message:"data not found"})
+        }
+    })
+)
 
 export default sellerOrderRouter;
