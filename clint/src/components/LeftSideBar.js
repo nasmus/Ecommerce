802a3fc9eee23155b0f4b0c8f577b2463a8237d7
nasmus <nasmus.shahadat@gmail.com>
2023-08-02 23:11:11 +0600

    code formation fix

--- clint/src/components/LeftSideBar.js
+++ clint/src/components/LeftSideBar.js
@@ -1,49 +1,37 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { getError } from '../utils';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { getError } from "../utils";
+import "../css/LeftSideBar.css";
 
 function LeftSideBar() {
-    const [Categories, setCategories] = useState([]);
+  const [Categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        const fatchCategories = async() => {
-            try{
-                const {data} = await axios.get(`/api/products/categories`);
-                setCategories(data);
-            } catch(err) {
-                toast.error(getError(err));
-            }
-        }
-        fatchCategories();
-    },[])
+  useEffect(() => {
+    const fatchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fatchCategories();
+  }, []);
   return (
-        <div className="ec-shop-leftside col-lg-3 col-md-12 order-lg-first order-md-last">
-                    <div id="shop_sidebar">
-                        <div className="ec-sidebar-heading">
-                            <h1>Filter By Category </h1>
-                        </div>
-                        <div className="ec-sidebar-wrap">
-                            <div className="ec-sidebar-block">                   
-                                <div className="ec-sb-block-content">
-                                    <ul>
-                                        {Categories.map((category) => (
-                                            <Link to={`/search?category=${category}`} >
-                                                <li key={category}  >                                                
-                                                    <div className="ec-sidebar-block-item ec-more-toggle">
-                                                        <span> <span><i class="fa-duotone fa-bars"></i></span> {category}</span>
-                                                    </div>
-                                                </li>
-                                            </Link>
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-  )
+    <div className="ec-shop-leftside col-lg-2 ">
+      <ul>
+        {Categories.map((category) => (
+          <Link className="Link_style" to={`/search?category=${category}`}>
+            <li key={category}>
+                <span className="items"> {category}</span>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default LeftSideBar
+export default LeftSideBar;
--- clint/src/components/Product.js
+++ clint/src/components/Product.js
@@ -1,71 +1,94 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 //import Button from 'react-bootstrap/Button'
-import Rating from './Rating';
-import { Store } from '../Store';
-import axios from 'axios';
-import Button from '@mui/material/Button';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Rating from "./Rating";
+import { Store } from "../Store";
+import axios from "axios";
+import Button from "@mui/material/Button";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 function Product(props) {
-    const { product } =props;
+  const { product } = props;
 
-    const { state, dispatch: ctxDispatch} = useContext(Store);
-    const {
-        cart:{ cartItems },
-    } = state
-    
-    const addToCartHandeler = async (item) => {
-      const existItem =  cartItems.find( x => x._id === product._id);
-      const quantity = existItem ? existItem.quantity + 1 : 1;
-      const { data } = await axios.get(`/api/products/${item._id}`)
-      if(data.countInStock < quantity){
-          window.alert('Sorry Bro !');
-          return ;
-      }
-      ctxDispatch({
-          type:'CART_ADD_ITEM',
-          payload:{...item,quantity}
-      })
-      
-  }
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandeler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry Bro !");
+      return;
+    }
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
 
   return (
-    
-      <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-6 mb-6 pro-gl-content">
-        <div className="ec-product-inner">
-          <div className="ec-pro-image-outer">
-            <div className="ec-pro-image">
+    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-6 mb-6 pro-gl-content">
+      <div className="ec-product-inner">
+        <div className="ec-pro-image-outer">
+          <div className="ec-pro-image">
             <Link to={`/product/${product.slug}`} className="image">
-              <img className="main-image"
-                src={product.image} alt={product.name} />
+              <img
+                className="main-image"
+                src={product.image}
+                alt={product.name}
+              />
             </Link>
-              <span className="percentage">Prime</span>
+            <span className="percentage">Prime</span>
+          </div>
+        </div>
+        <div className="ec-pro-content">
+          <h5 className="ec-pro-title">
+            <Link to={`/product/${product.slug}`}>{product.name}</Link>
+          </h5>
+          <div className="ec-pro-rating">
+            <span>
+              {" "}
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              />{" "}
+            </span>
+          </div>
+          <span className="ec-price">
+            <span className="new-price">${product.price}</span>
+          </span>
+          <div className="ec-pro-option">
+            <div className="ec-pro-size">
+              <span className="ec-pro-opt-label">Button</span>
+              {product.countInStock === 0 ? (
+                <Button
+                  variant="contained"
+                  size="small"
+                  endIcon={<AddShoppingCartIcon />}
+                  disabled
+                >
+                  Out Of Stock
+                </Button>
+              ) : (
+                <Button
+                  onClick={() => addToCartHandeler(product)}
+                  variant="contained"
+                  size="small"
+                  endIcon={<AddShoppingCartIcon />}
+                >
+                  Add To Cart
+                </Button>
+              )}
             </div>
           </div>
-              <div className="ec-pro-content">
-                <h5 className="ec-pro-title"><Link to={`/product/${product.slug}`}>{product.name}</Link></h5>
-                  <div className="ec-pro-rating">
-                    <span> <Rating rating={product.rating} numReviews={product.numReviews} /> </span>
-                  </div>
-                    <span className="ec-price">
-                    <span className="new-price">${product.price}</span>
-                    </span>
-                      <div className="ec-pro-option">
-                        <div className="ec-pro-size">
-                          <span className="ec-pro-opt-label">Button</span>
-                          {
-                            product.countInStock === 0 
-                            ? <Button variant='contained' size="small" endIcon={<AddShoppingCartIcon />} disabled >Out Of Stock</Button>
-                            : <Button onClick={() => addToCartHandeler(product)} variant="contained" size="small" endIcon={<AddShoppingCartIcon />} >Add To Cart</Button>
-                          }
-                        </div>
-                    </div>
-              </div>
-          </div>
-      </div>          
-  )
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
