9b9cbdec7ef875a60bf4a63b36400e41e0536b22
nasmus <nasmus.shahadat@gmail.com>
2023-09-08 11:06:22 +0600

    admin all order screen done

---
+++ admin/src/AdminScreen/AllOrderScreen.js
@@ -0,0 +1,53 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Store } from "../Store";
+import Sidebar from "../Component/Sidebar";
+
+function AllOrderScreen() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const [allOrder, setAllOrder] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fatchData = async () => {
+      const orderList = await axios.get(`/api/admin/orderlist/allorder`, {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      });
+      if(orderList) {
+        setAllOrder(orderList.data);
+      } else {
+        console.log("order not found");
+      }
+    };
+    fatchData();
+  }, [userInfo.token]);
+  return (
+    <div>
+      <Sidebar />
+      {console.log(allOrder)}
+      <div>
+        <table>
+          <tr>
+            <th>Order ID</th>
+            <th>Payment</th>
+            <th>itemsPrice</th>
+            <th>user ID</th>
+            <th>createdAt</th>
+          </tr>
+          {allOrder.map((item,index) => (
+            <tr key={index} >
+              <td>{item._id}</td>
+              <td>{item.paymentMethod}</td>
+              <td>{item.itemsPrice}</td>
+              <td>{item.user}</td>
+              <td>{item.createdAt}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default AllOrderScreen;
