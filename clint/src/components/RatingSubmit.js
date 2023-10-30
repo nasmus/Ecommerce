9b221609bf1bca9ad6c4933b4550f52fd021b0ae
nasmus <nasmus.shahadat@gmail.com>
2023-10-30 21:37:50 +0600

    product rating logic fix

--- clint/src/components/RatingSubmit.js
+++ clint/src/components/RatingSubmit.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState,useReducer } from "react";
-import { Alert, Rating } from "@mui/material";
+import React, { useContext, useState,useReducer, useEffect } from "react";
+import { Rating } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Store } from "../Store";
 import { toast } from "react-toastify";
@@ -9,7 +9,6 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'CREATE_SUCCESS':
       return { ...state };
-    
     default:
       return state;
   }
@@ -20,6 +19,7 @@ function RatingSubmit(props) {
   const navigate = useNavigate();
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
+  const [delevaryStatus, setDelevaryStatus] = useState(false)
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
 
@@ -29,6 +29,19 @@ function RatingSubmit(props) {
       loading: true,
       error: '',
     });
+
+  useEffect(() => {
+    const fatchData = async() =>{
+      const status = await axios.get(`/api/delevary_status/${userInfo._id}/status/${props.product._id}`)
+      if(status.data === 'panding'){
+        setDelevaryStatus(true)
+      } else{
+        setDelevaryStatus(false)
+      }
+      
+    }
+    fatchData()
+  },[props.product._id,userInfo._id])
 
   const submitHandle = async (e) => {
     e.preventDefault();
@@ -62,15 +75,16 @@ function RatingSubmit(props) {
     {
       userInfo ? <div className="lg:w-2/3 m-3">
       <form onSubmit={submitHandle}>
-        <div class="w-full mb-4 border-2 border-gray-200 rounded-lg bg-gray-50">
-          <div class="px-4 py-2 bg-white rounded-t-lg ">
-            <label for="comment" class="sr-only">
+        {console.log(delevaryStatus)}
+        <div className="w-full mb-4 border-2 border-gray-200 rounded-lg bg-gray-50">
+          <div className="px-4 py-2 bg-white rounded-t-lg ">
+            <label for="comment" className="sr-only">
               Your comment
             </label>
             <textarea
               id="comment"
               rows="4"
-              class="w-full px-0 text-sm text-gray-900 bg-white border-0  focus:ring-0"
+              className="w-full px-0 text-sm text-gray-900 bg-white border-0  focus:ring-0"
               placeholder="Write a comment..."
               required
               onChange={(e) => setComment(e.target.value)}
@@ -85,10 +99,10 @@ function RatingSubmit(props) {
               }}
             />
           </div>
-          <div class="flex items-center justify-between px-3 py-2 border-t ">
+          <div className="flex items-center justify-between px-3 py-2 border-t ">
             <button
               type="submit"
-              class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-600"
+              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-600"
             >
               Post comment
             </button>
